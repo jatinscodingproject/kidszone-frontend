ea0606f317664b790d59018f98199b491b131180
jatinscodingproject <jatincoder104@gmail.com>
2026-02-16 05:33:51 -0800

    code update

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -2,8 +2,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
 
 // 👇 Import all images dynamically
 const images = Object.values(
